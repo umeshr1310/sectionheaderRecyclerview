070d097cd590773e62ce7a7fb994bfb9b7d33348
Umesh <r.umeshr84@gmail.com>
2019-01-13 21:21:27 +0530

    Initial commit

---
+++ app/src/main/java/com/umesh/lettersectionedrecyclerview/CountriesModel.java
@@ -0,0 +1,15 @@
+package com.umesh.lettersectionedrecyclerview;
+
+/**
+ * Created by Sayan Manna on 13-07-2017.
+ */
+
+public class CountriesModel {
+    public String name;
+    public boolean isSection;
+
+    public CountriesModel(String name, boolean isSection) {
+        this.name = name;
+        this.isSection = isSection;
+    }
+}
